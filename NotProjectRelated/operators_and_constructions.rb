603b16574daa890538e5374e0c51fcfb7c90664d
VShcherba <vlad.shcherba@gmail.com>
2016-07-12 13:49:36 +0300

    practice task for constructions is done

---
+++ NotProjectRelated/operators_and_constructions.rb
@@ -0,0 +1,41 @@
+=begin
+1. Create method which gets 10 random integers (from 0 to 9) (generate it using while or until)
+
+before output for each next number we should puts "========="
+                if number is even or odd it should puts string "the number is even" or "the number is odd"
+                  if it's more (or less) than 5 it should puts appropriate string "the number is more (or less) than 5"
+in case we've got "0" we should puts "we've got 0"
+                  in each case we should get 2 messages except the '0' case
+                  2. add to your solution next test
+
+                  create project
+                  make a random action: create or not a new bug issue
+                  open project page,
+                               open issues tab
+                  if there is a bug present then add self to watchers
+                  if no then create a new bug issue and then add self to watchers
+                  Verify that there is a bug issue and that current user is a watcher
+=end
+i = 0
+while i < 10
+  i +=1
+  num = rand(9)
+  puts num
+  if num == 0
+    puts "we've got 0"
+  elsif num.odd?
+    puts 'the number is odd'
+  elsif num.even?
+    puts 'the number is even'
+  end
+
+
+  if  num < 5 and num > 0
+    puts 'the number is less than 5'
+  elsif num > 5
+    puts 'the number is more than 5'
+  elsif num == 5
+    puts "that's five"
+  end
+  puts '========='
+end
